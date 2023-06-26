3b838d3e6aa06141fa786fb2319612d2246b6e9c
Paller1337 <max.paller@yandex.ru>
2023-06-26 18:23:45 +0300

    room img update

--- data/hotelRooms.ts
+++ data/hotelRooms.ts
@@ -6,11 +6,14 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Домик на набережной',
         description: 'Уютный домик на набережной, включающий в себя мини-кухню, душевую, спальную и зал в панорамными окнами.',
         images: [
-            '/img/hotel/room-previews/module-house.jpg',
-            '/img/hotel/room-previews/austr-townhouse.jpg',
-            '/img/hotel/room-previews/deluxe-5-corps.jpg',
-            '/img/hotel/room-previews/comfort-sauna.jpg',
-            '/img/hotel/room-previews/sports-corps-2.jpg',
+            '/img/hotel/room-previews/module-house/1.jpg',
+            '/img/hotel/room-previews/module-house/2.jpg',
+            '/img/hotel/room-previews/module-house/3.jpg',
+            '/img/hotel/room-previews/module-house/4.jpg',
+            '/img/hotel/room-previews/module-house/5.jpg',
+            '/img/hotel/room-previews/module-house/6.jpg',
+            '/img/hotel/room-previews/module-house/7.jpg',
+            '/img/hotel/room-previews/module-house/8.jpg',
         ],
         size: 'Размер номера 48 м².', // Добавить площадь терассы 9 кв.м.
         attributes: [
@@ -35,7 +38,16 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Австрийский таунхаус',
         description: 'Двухэтажный таунхаус в современном стиле с мини-кухней, баней. Два варианта размещения: с русской парной или финской сауной.',
         images: [
-            '/img/hotel/room-previews/austr-townhouse.jpg',
+            '/img/hotel/room-previews/austr-townhouse/1.jpg',
+            '/img/hotel/room-previews/austr-townhouse/2.jpg',
+            '/img/hotel/room-previews/austr-townhouse/3.jpg',
+            '/img/hotel/room-previews/austr-townhouse/4.jpg',
+            '/img/hotel/room-previews/austr-townhouse/5.jpg',
+            '/img/hotel/room-previews/austr-townhouse/6.jpg',
+            '/img/hotel/room-previews/austr-townhouse/7.jpg',
+            '/img/hotel/room-previews/austr-townhouse/8.jpg',
+            '/img/hotel/room-previews/austr-townhouse/9.jpg',
+            '/img/hotel/room-previews/austr-townhouse/10.jpg',
         ],
         size: 'Размер номера 145 м².',
         attributes: [
@@ -60,7 +72,11 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Делюкс на набережной',
         description: 'Новый корпус на набережной с прекрасным видом на водоём.',
         images: [
-            '/img/hotel/room-previews/deluxe-5-corps.jpg',
+            '/img/hotel/room-previews/deluxe-5-corps/1.jpg',
+            '/img/hotel/room-previews/deluxe-5-corps/2.jpg',
+            '/img/hotel/room-previews/deluxe-5-corps/3.jpg',
+            '/img/hotel/room-previews/deluxe-5-corps/4.jpg',
+            '/img/hotel/room-previews/deluxe-5-corps/5.jpg',
         ],
         size: 'Размер номера 26 м².',
         attributes: [
@@ -85,7 +101,13 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Номер Комфорт с сауной',
         description: 'Однокомнатный номер для двоих с сауной.',
         images: [
-            '/img/hotel/room-previews/comfort-sauna.jpg',
+            '/img/hotel/room-previews/comfort-sauna/1.jpg',
+            '/img/hotel/room-previews/comfort-sauna/2.jpg',
+            '/img/hotel/room-previews/comfort-sauna/3.jpg',
+            '/img/hotel/room-previews/comfort-sauna/4.jpg',
+            '/img/hotel/room-previews/comfort-sauna/5.jpg',
+            '/img/hotel/room-previews/comfort-sauna/6.jpg',
+            '/img/hotel/room-previews/comfort-sauna/7.jpg',
         ],
         size: 'Размер номера 50 м².',
         attributes: [
@@ -110,7 +132,13 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Евростандарт спортивный корпус 2 этаж',
         description: 'Однокомнатный номер на двоих. Корпус находится в 30 метрах от основной территории парк-отеля через дорогу.',
         images: [
-            '/img/hotel/room-previews/sports-corps-2.jpg',
+            '/img/hotel/room-previews/sports-corps-2/1.jpg',
+            '/img/hotel/room-previews/sports-corps-2/2.jpg',
+            '/img/hotel/room-previews/sports-corps-2/3.jpg',
+            '/img/hotel/room-previews/sports-corps-2/4.jpg',
+            '/img/hotel/room-previews/sports-corps-2/5.jpg',
+            '/img/hotel/room-previews/sports-corps-2/6.jpg',
+            '/img/hotel/room-previews/sports-corps-2/7.jpg',
         ],
         size: 'Размеры номеров 13 м² и 18 м².',
         attributes: [
@@ -135,7 +163,12 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Номер Комфорт',
         description: 'Однокомнатный номер для двоих. Отдельный вход с верандой.',
         images: [
-            '/img/hotel/room-previews/comfort.jpeg'
+            '/img/hotel/room-previews/comfort/1.jpg',
+            '/img/hotel/room-previews/comfort/2.jpg',
+            '/img/hotel/room-previews/comfort/3.jpg',
+            '/img/hotel/room-previews/comfort/4.jpg',
+            '/img/hotel/room-previews/comfort/5.jpg',
+            '/img/hotel/room-previews/comfort/6.jpg',
         ],
         size: 'Размер номера 33 м².',
         attributes: [
@@ -160,7 +193,13 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Номер Комфорт с сауной',
         description: 'Однокомнатный номер для двоих. Отдельный вход с верандой.',
         images: [
-            '/img/hotel/room-previews/comfort.jpeg',
+            '/img/hotel/room-previews/comfort-sauna/1.jpg',
+            '/img/hotel/room-previews/comfort-sauna/2.jpg',
+            '/img/hotel/room-previews/comfort-sauna/3.jpg',
+            '/img/hotel/room-previews/comfort-sauna/4.jpg',
+            '/img/hotel/room-previews/comfort-sauna/5.jpg',
+            '/img/hotel/room-previews/comfort-sauna/6.jpg',
+            '/img/hotel/room-previews/comfort-sauna/7.jpg',
         ],
         size: 'Размер номера 33 м².',
         attributes: [
@@ -186,7 +225,12 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Коттедж',
         description: 'Просторный номер класса "Люкс" с панорамным видом на море.',
         images: [
-            '/img/hotel/room-previews/cottage.jpeg',
+            '/img/hotel/room-previews/cottage/1.jpg',
+            '/img/hotel/room-previews/cottage/2.jpg',
+            '/img/hotel/room-previews/cottage/3.jpg',
+            '/img/hotel/room-previews/cottage/4.jpg',
+            '/img/hotel/room-previews/cottage/5.jpg',
+            '/img/hotel/room-previews/cottage/6.jpg',
         ],
         size: 'Размер номера 64 м².',
         attributes: [
@@ -211,7 +255,13 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Таунхаус',
         description: 'Просторный номер класса "Люкс" с панорамным видом на море.',
         images: [
-            '/img/hotel/room-previews/townhouse.jpeg',
+            '/img/hotel/room-previews/townhouse/1.jpg',
+            '/img/hotel/room-previews/townhouse/2.jpg',
+            '/img/hotel/room-previews/townhouse/3.jpg',
+            '/img/hotel/room-previews/townhouse/4.jpg',
+            '/img/hotel/room-previews/townhouse/5.jpg',
+            '/img/hotel/room-previews/townhouse/6.jpg',
+            '/img/hotel/room-previews/townhouse/7.jpg',
         ],
         size: 'Размер номера 96 м².',
         attributes: [
@@ -231,37 +281,42 @@ export const hotelRooms: RoomObjectProps[] = [
             'Доступ в интернет'
         ]
     },
-    {
-        id: 16542,
-        title: 'Евростандарт',
-        description: 'Просторный номер класса "Люкс" с панорамным видом на море.',
-        images: [
-            '/img/hotel/room-previews/eurostandard.jpeg',
-        ],
-        size: 'Размер номера 25 м².',
-        attributes: [
-            { name: 'Цены', value: '<b>2850 руб/сутки</b>' },
-            { name: 'Вместимость', value: '2 человека.' }
-        ],
-        amenities: [
-            'Банные полотенца и полотенца для рук',
-            'Фен',
-            'Чайник',
-            'Кофеварка',
-            'Мини-бар',
-            'Кондиционер',
-            'Столовые приборы и посуда',
-            'Питьевая вода',
-            'Телевизор',
-            'Доступ в интернет'
-        ]
-    },
+    // {
+    //     id: 16542,
+    //     title: 'Евростандарт',
+    //     description: 'Просторный номер класса "Люкс" с панорамным видом на море.',
+    //     images: [
+    //         '/img/hotel/room-previews/eurostandard.jpeg',
+    //     ],
+    //     size: 'Размер номера 25 м².',
+    //     attributes: [
+    //         { name: 'Цены', value: '<b>2850 руб/сутки</b>' },
+    //         { name: 'Вместимость', value: '2 человека.' }
+    //     ],
+    //     amenities: [
+    //         'Банные полотенца и полотенца для рук',
+    //         'Фен',
+    //         'Чайник',
+    //         'Кофеварка',
+    //         'Мини-бар',
+    //         'Кондиционер',
+    //         'Столовые приборы и посуда',
+    //         'Питьевая вода',
+    //         'Телевизор',
+    //         'Доступ в интернет'
+    //     ]
+    // },
     {
         id: [16543,16544],
         title: 'Делюкс с балконом',
         description: 'Однокомнатный номер на двоих с большим балконом.',
         images: [
-            '/img/hotel/room-previews/deluxe-balcony.jpeg',
+            '/img/hotel/room-previews/deluxe-balcony/1.jpg',
+            '/img/hotel/room-previews/deluxe-balcony/2.jpg',
+            '/img/hotel/room-previews/deluxe-balcony/3.jpg',
+            '/img/hotel/room-previews/deluxe-balcony/4.jpg',
+            '/img/hotel/room-previews/deluxe-balcony/5.jpg',
+            '/img/hotel/room-previews/deluxe-balcony/6.jpg',
         ],
         size: 'Размер номера 25 м².',
         attributes: [
@@ -286,7 +341,9 @@ export const hotelRooms: RoomObjectProps[] = [
         title: 'Евростандарт спортивный корпус',
         description: 'Однокомнатный номер на двоих. Корпус находится в 30 метрах от основной территории парк-отеля через дорогу.',
         images: [
-            '/img/hotel/room-previews/eurostandard-sport-corps.jpeg',
+            '/img/hotel/room-previews/eurostandard-sport-corps/1.jpg',
+            '/img/hotel/room-previews/eurostandard-sport-corps/2.jpg',
+            '/img/hotel/room-previews/eurostandard-sport-corps/3.jpg',
         ],
         size: 'Размеры номеров от 12,7 м² до 15 м².',
         attributes: [
