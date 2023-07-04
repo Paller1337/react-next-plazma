7e36da5da32c9aa5ba5d1caa690df9280a106a3c
Paller1337 <max.paller@yandex.ru>
2023-07-04 17:17:30 +0300

    rooms loading

--- components/objects/RoomObject.tsx
+++ components/objects/RoomObject.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { Suspense, useEffect, useRef, useState } from 'react'
 import BookingRoom from '../bnovo/BookingRoom'
 import GallerySlider from '../GallerySlider'
 import Image, { StaticImageData } from 'next/image'
@@ -17,6 +17,10 @@ export interface RoomObjectProps {
     amenities: string[]
 }
 
+export const Loading = () => {
+
+    return <p>Loading</p>
+}
 export default function RoomObject(data: RoomObjectProps) {
     const images = data.images
 
@@ -70,68 +74,70 @@ export default function RoomObject(data: RoomObjectProps) {
             isOpen={galleryIsOpen}
             onClose={closeGallery}
         />
-        <div className='hotel-rooms__item hotel-room' key={data.id.toString()}>
-            <div className='hotel-room__preview'>
-                <div className='hotel-room__image-wrapper'>
-                    <div className='hotel-room__image-images'>
-                        <div ref={imageContentRef} className='hotel-room__image-content' style={{ transform: `translateX(-${translate}px)` }}>
-                            {images && images.map((image, i) =>
-                                <div key={i} className={`hotel-room__image`} onClick={() => setGalleryIsOpen(true)}>
-                                    <Image key={i} src={image} height={0} width={0} alt={'Plazma'}
-                                    />
-                                </div >
+        <Suspense fallback={() => <Loading/>}>
+            <div className='hotel-rooms__item hotel-room' key={data.id.toString()}>
+                <div className='hotel-room__preview'>
+                    <div className='hotel-room__image-wrapper'>
+                        <div className='hotel-room__image-images'>
+                            <div ref={imageContentRef} className='hotel-room__image-content' style={{ transform: `translateX(-${translate}px)` }}>
+                                {images && images.map((image, i) =>
+                                    <div key={i} className={`hotel-room__image`} onClick={() => setGalleryIsOpen(true)}>
+                                        <Image key={i} src={image} height={330} width={570} alt={'Plazma'}
+                                        />
+                                    </div >
+                                )}
+                            </div>
+                        </div>
+                        <button className='hotel-room__image--prev' onClick={(e) => prevSlide(e)}></button>
+                        <button className='hotel-room__image--next' onClick={(e) => nextSlide(e)}></button>
+                    </div>
+                    {images && images.length > 1 ?
+                        <div className='hotel-room__slides'>
+                            {images.map((x, i) =>
+                                <div key={i} className='hotel-room__slide' onClick={() => goSlide(i)}>
+
+                                    <Image src={x} height={60} width={80} alt={'Plazma гостиница'} />
+
+                                </div>
                             )}
                         </div>
-                    </div>
-                    <button className='hotel-room__image--prev' onClick={(e) => prevSlide(e)}></button>
-                    <button className='hotel-room__image--next' onClick={(e) => nextSlide(e)}></button>
+                        : ''}
                 </div>
-                {images && images.length > 1 ?
-                    <div className='hotel-room__slides'>
-                        {images.map((x, i) =>
-                            <div key={i} className='hotel-room__slide' onClick={() => goSlide(i)}>
 
-                                <Image src={x} height={60} width={80} alt={'Plazma гостиница'} />
+                <div className='hotel-room__info'>
+                    <span className='hotel-room__title'>{data.title}</span>
+                    <span className='hotel-room__text'>{data.description}
+                    </span>
+                    <span className='hotel-room__text bold'>{data.size}</span>
 
+                    {/* <div className='btn booking-btn'>Забронировать</div> */}
+                    <BookingRoom roomId={data.id} targetId={`${data.id}-target`} />
+                    <div className='hotel-room__attrs'>
+                        {data.attributes.map((x, i) =>
+                            <div key={i} className='hotel-room__attr-item'>
+                                <span className='hotel-room__text attr-name'>{x.name}</span>
+                                <span className='hotel-room__text attr-value'>
+                                    {x.value.map((text, i) =>
+                                        <span key={i} dangerouslySetInnerHTML={{ __html: text }} />
+                                    )}
+                                </span>
                             </div>
                         )}
+                    </div >
+
+
+                    <div className='hotel-room__amenities'>
+                        <span onClick={() => setAmenitiesOpened(current => !current)} className='hotel-room__text amenities-title'>Удобства в номере:
+
+                            <i className='hotel-arrow'></i>
+                        </span>
+                        <ul className={`hotel-room__amenities-list ${amenitiesOpened ? 'opened' : ''}`}>
+                            {data.amenities.map((x, i) => <li key={i}>{x}</li>)}
+                        </ul>
                     </div>
-                    : ''}
-            </div>
 
-            <div className='hotel-room__info'>
-                <span className='hotel-room__title'>{data.title}</span>
-                <span className='hotel-room__text'>{data.description}
-                </span>
-                <span className='hotel-room__text bold'>{data.size}</span>
-
-                {/* <div className='btn booking-btn'>Забронировать</div> */}
-                <BookingRoom roomId={data.id} targetId={`${data.id}-target`} />
-                <div className='hotel-room__attrs'>
-                    {data.attributes.map((x, i) =>
-                        <div key={i} className='hotel-room__attr-item'>
-                            <span className='hotel-room__text attr-name'>{x.name}</span>
-                            <span className='hotel-room__text attr-value'>
-                                {x.value.map((text, i) =>
-                                    <span key={i} dangerouslySetInnerHTML={{ __html: text }} />
-                                )}
-                            </span>
-                        </div>
-                    )}
                 </div >
-
-
-                <div className='hotel-room__amenities'>
-                    <span onClick={() => setAmenitiesOpened(current => !current)} className='hotel-room__text amenities-title'>Удобства в номере:
-
-                        <i className='hotel-arrow'></i>
-                    </span>
-                    <ul className={`hotel-room__amenities-list ${amenitiesOpened ? 'opened' : ''}`}>
-                        {data.amenities.map((x, i) => <li key={i}>{x}</li>)}
-                    </ul>
-                </div>
-
             </div >
-        </div >
+        </Suspense>
     </>)
 }
