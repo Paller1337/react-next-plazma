6e41c77db399e7910484a21a3194a6ff05a4e207
Paller1337 <max.paller@yandex.ru>
2025-03-19 11:19:39 +0300

    Georgia fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ export default function Document() {
                     <link rel='preconnect' href='https://fonts.googleapis.com' />
                     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet" />
-                    <link href="https://fonts.googleapis.com/css2?family=Georgia:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet" />
+                    {/* <link href="https://fonts.googleapis.com/css2?family=Georgia:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap" rel="stylesheet" /> */}
 
                     <meta name="google-site-verification" content="E36ptUCTRfUBdjkHz434f1EO1Waq_Jy98S5lWI4qj_E" />
                 </Head>
